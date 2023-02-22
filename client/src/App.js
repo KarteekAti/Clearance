d51d4fa6d009e032702103d48708759879a1c549
sablesayali <sayalimsable25@gmail.com>
2023-02-22 13:01:43 +0530

    created login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
+import React, { useState } from "react";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 
-
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
   return (
     <div className="App">
-
+      {
+        currentForm === "login" ? <login /> : <Register />
+      }
+      <Login />
     </div>
   );
 }
--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Login() {
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="flex justify-center items-center h-screen">
+      <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+            Email
+          </label>
+          <input
+            value={email}
+            type="email"
+            placeholder="Enter your email"
+            id="email"
+            name="email"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            Password
+          </label>
+          <input
+            value={password}
+            type="password"
+            placeholder="Enter your password"
+            id="password"
+            name="password"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Log In
+          </button>
+          <button
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          >
+            Don't have an account? Register here.
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
-
-export default Login
