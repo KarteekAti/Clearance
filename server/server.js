60c9e6982e90dd02be52d7dff61cddc0fb8bfed8
karteekati <ati.karteek99@gmail.com>
2023-02-22 12:08:37 +0530

    server changes

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
