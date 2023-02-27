449aca831c13cf915b78370579d38e9c6729979c
Karteek <ati.karteek99@gmail.com>
2023-02-27 10:00:05 +0530

    keys update

--- server/firebase.js
+++ server/firebase.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from './clearance-9ee70-firebase-adminsdk-4vinx-a389552355.json' assert { type: "json" };
+import serviceAccount from './keys.json' assert { type: "json" };
 
 const app = admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
 
